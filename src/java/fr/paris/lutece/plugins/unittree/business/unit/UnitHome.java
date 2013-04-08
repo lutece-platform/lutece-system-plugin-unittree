b097f4c7b6c5c889a7fb6abb37d7e92f32740023
vbroussard <vbroussard@users.noreply.github.com>
2013-04-08 07:24:16 +0000

    UNITTREE-12 : Correction of javadoc.
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/system/plugin-unittree@47635 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/unittree/business/unit/UnitHome.java
+++ src/java/fr/paris/lutece/plugins/unittree/business/unit/UnitHome.java
@@ -218,7 +218,6 @@ public final class UnitHome
      * Update the parent of a unit
      * @param nIdUnitToMove The id of the unit to change the parent of.
      * @param nIdNewParent The id of the new parent of the unit.
-     * @param _plugin The plugin
      */
     public static void updateParent( int nIdUnitToMove, int nIdNewParent )
     {
