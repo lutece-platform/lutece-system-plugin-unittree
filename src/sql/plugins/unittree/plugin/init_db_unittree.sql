b3bd3d78da4e82584c494bd892b4ea345b0c4cc0
Laurent HOHL <laurent.hohl@paris.fr>
2016-01-28 16:46:45 +0100

    UNITTREE-18 - Set correct icons for actions

--- src/sql/plugins/unittree/plugin/init_db_unittree.sql
+++ src/sql/plugins/unittree/plugin/init_db_unittree.sql
@@ -7,18 +7,10 @@ INSERT INTO unittree_unit (id_unit, id_parent, label, description) VALUES (0,-1,
 --
 -- Dumping data for table unittree_unit_action
 --
-INSERT INTO unittree_action (id_action,name_key,description_key,action_url,icon_url,action_permission,action_type) VALUES 
-(1,'unittree.unit.action.createUnit.name','unittree.unit.action.createUnit.description','jsp/admin/plugins/unittree/CreateUnit.jsp','icon-plus','CREATE', 'unittree.unitAction');
-INSERT INTO unittree_action (id_action,name_key,description_key,action_url,icon_url,action_permission,action_type) VALUES 
-(2,'unittree.unit.action.modifyUnit.name','unittree.unit.action.modifyUnit.description','jsp/admin/plugins/unittree/ModifyUnit.jsp','icon-edit','MODIFY', 'unittree.unitAction');
-INSERT INTO unittree_action (id_action,name_key,description_key,action_url,icon_url,action_permission,action_type) VALUES 
-(3,'unittree.unit.action.deleteUnit.name','unittree.unit.action.deleteUnit.description','jsp/admin/plugins/unittree/RemoveUnit.jsp','icon-trash','DELETE', 'unittree.unitAction');
-INSERT INTO unittree_action (id_action,name_key,description_key,action_url,icon_url,action_permission,action_type) VALUES 
-(4,'unittree.user.action.modifyUser.name','unittree.user.action.modifyUser.description','jsp/admin/plugins/unittree/ModifyUser.jsp','icon-edit','MODIFY_USER', 'unittree.unitUserAction');
-INSERT INTO unittree_action (id_action,name_key,description_key,action_url,icon_url,action_permission,action_type) VALUES 
-(5,'unittree.user.action.moveUser.name','unittree.user.action.moveUser.description','jsp/admin/plugins/unittree/MoveUser.jsp','icon-share','MOVE_USER', 'unittree.unitUserAction');
-INSERT INTO unittree_action (id_action,name_key,description_key,action_url,icon_url,action_permission,action_type) VALUES 
-(6,'unittree.user.action.removeUser.name','unittree.user.action.removeUser.description','jsp/admin/plugins/unittree/RemoveUser.jsp','icon-trash','REMOVE_USER', 'unittree.unitUserAction');
-
-INSERT INTO unittree_action (id_action,name_key,description_key,action_url,icon_url,action_permission,action_type) VALUES 
-(10,'unittree.unit.action.moveSubTree.name','unittree.unit.action.moveSubTree.description','jsp/admin/plugins/unittree/MoveSubTree.jsp','icon-share-alt','MOVE_SUB_TREE', 'unittree.unitAction');
+INSERT INTO unittree_action VALUES (1, 'unittree.unit.action.createUnit.name', 'unittree.unit.action.createUnit.description', 'jsp/admin/plugins/unittree/CreateUnit.jsp', 'fa fa-plus fa-fw', 'CREATE', 'unittree.unitAction');
+INSERT INTO unittree_action VALUES (2, 'unittree.unit.action.modifyUnit.name', 'unittree.unit.action.modifyUnit.description', 'jsp/admin/plugins/unittree/ModifyUnit.jsp', 'fa fa-pencil fa-fw', 'MODIFY', 'unittree.unitAction');
+INSERT INTO unittree_action VALUES (3, 'unittree.unit.action.deleteUnit.name', 'unittree.unit.action.deleteUnit.description', 'jsp/admin/plugins/unittree/RemoveUnit.jsp', 'fa fa-trash fa-fw', 'DELETE', 'unittree.unitAction');
+INSERT INTO unittree_action VALUES (4, 'unittree.user.action.modifyUser.name', 'unittree.user.action.modifyUser.description', 'jsp/admin/plugins/unittree/ModifyUser.jsp', 'fa fa-pencil fa-fw', 'MODIFY_USER', 'unittree.unitUserAction');
+INSERT INTO unittree_action VALUES (5, 'unittree.user.action.moveUser.name', 'unittree.user.action.moveUser.description', 'jsp/admin/plugins/unittree/MoveUser.jsp', 'fa fa-sort fa-fw', 'MOVE_USER', 'unittree.unitUserAction');
+INSERT INTO unittree_action VALUES (6, 'unittree.user.action.removeUser.name', 'unittree.user.action.removeUser.description', 'jsp/admin/plugins/unittree/RemoveUser.jsp', 'fa fa-user-times fa-fw', 'REMOVE_USER', 'unittree.unitUserAction');
+INSERT INTO unittree_action VALUES (10, 'unittree.unit.action.moveSubTree.name', 'unittree.unit.action.moveSubTree.description', 'jsp/admin/plugins/unittree/MoveSubTree.jsp', 'fa fa-sort fa-fw', 'MOVE_SUB_TREE', 'unittree.unitAction');
