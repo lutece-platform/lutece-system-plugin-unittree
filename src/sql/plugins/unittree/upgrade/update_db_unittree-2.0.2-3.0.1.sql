af6f314d9967f0535ba92f0fd68bcf4f7e21b281
Elysa Jouve <elysa.jouve@paris.fr>
2020-12-02 17:19:29 +0200

    UNITTREE-39: edited icons in sql files for core v7 compatibility

---
+++ src/sql/plugins/unittree/upgrade/update_db_unittree-2.0.2-3.0.1.sql
@@ -0,0 +1,7 @@
+UPDATE unittree_action SET icon_url='plus' WHERE id_action=1;
+UPDATE unittree_action SET icon_url='edit' WHERE id_action=2;
+UPDATE unittree_action SET icon_url='trash' WHERE id_action=3;
+UPDATE unittree_action SET icon_url='edit' WHERE id_action=4;
+UPDATE unittree_action SET icon_url='sort' WHERE id_action=5;
+UPDATE unittree_action SET icon_url='user-times' WHERE id_action=6;
+UPDATE unittree_action SET icon_url='sort' WHERE id_action=10;
