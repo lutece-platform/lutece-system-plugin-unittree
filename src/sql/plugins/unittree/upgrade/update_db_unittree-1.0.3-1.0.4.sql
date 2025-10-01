00d67d7cb68b2eff5f7627ef2adfbd28eb0aac48
Anas BENJELLOUN <anasbenjellountouimi@gmail.com>
2025-10-01 11:12:26 +0200

    LUT-25806 Initialisation script cannot be run twice with mariadb + Liquibase

--- src/sql/plugins/unittree/upgrade/update_db_unittree-1.0.3-1.0.4.sql
+++ src/sql/plugins/unittree/upgrade/update_db_unittree-1.0.3-1.0.4.sql
@@ -1,5 +1,5 @@
---liquibase formatted sql
---changeset unittree:update_db_unittree-1.0.3-1.0.4.sql
---preconditions onFail:MARK_RAN onError:WARN
+-- liquibase formatted sql
+-- changeset unittree:update_db_unittree-1.0.3-1.0.4.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 INSERT INTO unittree_action (id_action,name_key,description_key,action_url,icon_url,action_permission,action_type) VALUES 
 (10,'unittree.unit.action.moveSubTree.name','unittree.unit.action.moveSubTree.description','jsp/admin/plugins/unittree/MoveSubTree.jsp','images/admin/skin/plugins/unittree/actions/move_user.png','MOVE_SUB_TREE', 'unittree.unitAction');
--- src/sql/plugins/unittree/upgrade/update_db_unittree-1.0.4-2.0.0.sql
+++ src/sql/plugins/unittree/upgrade/update_db_unittree-1.0.4-2.0.0.sql
@@ -1,6 +1,6 @@
---liquibase formatted sql
---changeset unittree:update_db_unittree-1.0.4-2.0.0.sql
---preconditions onFail:MARK_RAN onError:WARN
+-- liquibase formatted sql
+-- changeset unittree:update_db_unittree-1.0.4-2.0.0.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 UPDATE unittree_action SET icon_url = 'icon-plus' WHERE id_action = 1;
 UPDATE unittree_action SET icon_url = 'icon-edit' WHERE id_action = 2;
 UPDATE unittree_action SET icon_url = 'icon-trash' WHERE id_action = 3;
